fdf3adee4b43b162059690dabfce653b0f965f65
Luciano dos santos <lucianodossantos1993@hotmail.com>
2018-05-24 15:35:36 -0300

    projeto iniciado

---
+++ Urionline/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Urionline
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int N;
+			int[,] mat;
+			int M = 8;
+			string resp = "S";
+			N = int.Parse(Console.ReadLine());
+
+			mat = new int[M, M];
+
+			for (int s = 0; s < N; s++)
+			{
+
+
+				for (int i = 0; i < M; i++)
+				{
+					string[] c = Console.ReadLine().Split(' ');
+					for (int j = 0; j < M; j++)
+					{
+						mat[i, j] = int.Parse(c[j]);
+					}
+				}
+
+				int a, b;
+
+				a = int.Parse(Console.ReadLine());
+				b = int.Parse(Console.ReadLine());
+				a--;
+				b--;
+				for (int j = 0; j < M; j++)
+				{
+					if (mat[a, j] == 1)
+					{
+						resp = "S";
+						M = 0;
+					}
+					else if (mat[j, b] == 1)
+					{
+						resp = "S";
+						M = 0;
+					}
+					else
+					{
+						resp = "N";
+					}
+				}
+
+				Console.WriteLine(resp);
+				M = 8;
+			}
+
+			Console.ReadLine();
+		}
+
+	}
+}
+
+
+
